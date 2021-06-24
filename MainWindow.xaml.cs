7ca22360134568af9254723a3f722e10e4d87d64
Mateusz Basiak <mateusz.basiak@gmail.com>
2021-06-24 22:11:10 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZodiacSign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private string getZosidacSign()
+        {
+            return "ryby";
+        }
+
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            string zodiacSign = getZosidacSign();
+            MessageBox.Show($"Witaj {txtName.Text}! Twój znak zodiaku to {zodiacSign}");
+        }
+
+        private void txtName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtName.Text = "";
+        }
+    }
+}
